e1b9461c3ef495951b7d8758e8931743e7cd1dab
Dedan Opiyo <dedan.opiyo@student.moringaschool.com>
2025-06-13 11:39:43 +0300

    Build RESTful APIs that are easy to navigate and use in applications. Develop a Flask API with successful frontend connections via fetch(). Integrate create and retrieve routes with the associated actions to return the appropriate JSON data.

--- server/app.py
+++ server/app.py
@@ -17,10 +17,61 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+
+        response = make_response(
+            plants,
+            200,
+        )
+
+        return response
+    
+    def post(self):
+        # # Option 1: through form
+        # new_plant = Plant(
+        #     name=request.form['name'],
+        #     image=request.form['image'],
+        #     price=request.form['price']
+        # )
+
+        # # Option 2: through JSON. get_json allows for error handling
+        data = request.get_json() # request.json also works, but it's not official. It's a shortcut property that calls get_json() under the hood.
+        
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price'] # Suppose we have/used db.Nmeric() in our column: Decimal(str(data['price'])) - accurate
+        )
+        
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response_dict = new_plant.to_dict()
+
+        response = make_response(
+            response_dict,
+            201,
+        )
+
+        return response
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+
+        plant = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            plant,
+            200,
+        )
+
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
